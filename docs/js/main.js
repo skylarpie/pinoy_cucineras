33e993bb81cb0f719b497979e82002665c429c2c
skylarpie <89561669+skylarpie@users.noreply.github.com>
2024-10-29 00:43:28 -0400

    Add files via upload

---
+++ docs/js/main.js
@@ -0,0 +1,5 @@
+// JavaScript to toggle menu visibility
+document.getElementById("burgerMenu").addEventListener("click", function() {
+    const slideMenu = document.getElementById("slideMenu");
+    slideMenu.classList.toggle("active");
+});
