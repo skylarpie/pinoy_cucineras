f42d1c595e3024704d7b4366beb52aa64e890ebe
skylarpie <89561669+skylarpie@users.noreply.github.com>
2024-10-29 00:47:07 -0400

    Delete docs directory

--- docs/js/main.js
+++
@@ -1,5 +0,0 @@
-// JavaScript to toggle menu visibility
-document.getElementById("burgerMenu").addEventListener("click", function() {
-    const slideMenu = document.getElementById("slideMenu");
-    slideMenu.classList.toggle("active");
-});
